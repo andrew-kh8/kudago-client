aca112ecce67da8b10eb16f865036c2798d85f2c
andrew-kh8 <horolskyandrey@gmail.com>
2025-08-09 13:21:05 +0300

    upd conn and request

--- lib/kudago/requests/base_request.rb
+++ lib/kudago/requests/base_request.rb
@@ -5,13 +5,19 @@ module Kudago
   module Requests
     class BaseRequest
       def self.get(path, params = {})
-        response = Connection.connection.get(path, params)
+        response = connection.get(path, params)
 
         if response.success?
           response.body
         else
           raise Kudago::Error.new(message: response.body[:detail], status: response.status, reason: response.reason_phrase)
         end
+      end
+
+      private_class_method
+
+      def self.connection
+        @@connection ||= Connection.connection
       end
     end
   end
